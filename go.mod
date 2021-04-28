9ab379c8eb3baded3db8865653bb8e70acc4ac83
Rachmat Priambudi <rachmat.keybladers93@gmail.com>
2021-04-28 16:07:49 +0700

    test

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kurosawa93/jfrog-go-rsch
+
+go 1.14
+
+require bitbucket.org/tunaiku/amargo-otel v0.17.4
---
+++ main.go
@@ -0,0 +1,19 @@
+package rsch
+
+import (
+	"context"
+	"fmt"
+
+	otel "bitbucket.org/tunaiku/amargo-otel"
+)
+
+func Diagnostic() {
+	fmt.Printf("Diagnosting OK!\n")
+
+	id := otel.GetTraceID(context.Background())
+	fmt.Printf("Trace ID: %v\n", id)
+}
+
+func Hello(name string) {
+	fmt.Printf("Hello, Sir " + name)
+}
